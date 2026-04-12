9878e768ba58267f6e149717d66684e299ea54bd
Marcus <marcuswilliamj@gmail.com>
2026-04-13 00:15:38 +0200

    Navbar work

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -4,7 +4,9 @@ import styles from "./NavBar.module.css"
 const navbar = () => {
   return (
     <div className={styles.navbar}>
-      
+      <div className={styles.container}>
+        <h2>ShopFlow</h2>
+      </div>
     </div>
   )
 }
