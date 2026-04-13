cd52e76d383e8acf87b22dc0d6f2fb794a5bb61d
Marcus <marcuswilliamj@gmail.com>
2026-04-13 15:01:27 +0200

    Nav changes and search bar added

--- apps/web/src/components/UI/Navigation/SearchBar.tsx
+++ apps/web/src/components/UI/Navigation/SearchBar.tsx
@@ -4,7 +4,7 @@ import styles from "./SearchBar.module.css"
 const SearchBar = () => {
   return (
     <div className={styles.searchBar}>
-        <p>Search...</p>
+        <input className={styles.inputField} type="text" placeholder='Search...'/>
     </div>
   )
 }
