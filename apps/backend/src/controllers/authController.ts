a9f4abc2405809a4b3dd1d64d7c7029f701516f4
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-04-15 18:58:25 +0200

    added a getProfile controller

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -1,14 +1,29 @@
 import { Request, Response, NextFunction } from "express";
-import * as authService from '../services/authService';
+import * as authService from "../services/authService";
 
 export async function loginUser(
     req: Request,
     res: Response,
-    next: NextFunction
+    next: NextFunction,
 ): Promise<void> {
     try {
-        const user = await authService.loginUser(req.body);
-        res.status(200).json(user)
+        const result = await authService.loginUser(req.body);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function getProfile(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<void> {
+    try {
+        res.status(200).json({
+            message: `Hello ${req.user?.username}`,
+            user: req.user,
+        });
     } catch (error) {
         next(error);
     }
