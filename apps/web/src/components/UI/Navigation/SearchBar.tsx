e9d42558705b035735750e59d037da4bbe1e5d98
Marcus <marcuswilliamj@gmail.com>
2026-04-15 13:30:47 +0200

    Search bar icon

--- apps/web/src/components/UI/Navigation/SearchBar.tsx
+++ apps/web/src/components/UI/Navigation/SearchBar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from "./SearchBar.module.css"
 
+import SearchIcon from "@/assets/icons/magnifying-glass-solid-full.svg?react"
+
 const SearchBar = () => {
   return (
     <div className={styles.searchBar}>
+        <SearchIcon className={styles.icon}/>
         <input className={styles.inputField} type="text" placeholder='Search...'/>
     </div>
   )
