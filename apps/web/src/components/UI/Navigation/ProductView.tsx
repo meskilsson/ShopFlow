28c63c099b30f48228a7581d0d73af7fd60f164b
Marcus <marcuswilliamj@gmail.com>
2026-04-15 13:53:37 +0200

    star

--- apps/web/src/components/UI/Navigation/ProductView.tsx
+++ apps/web/src/components/UI/Navigation/ProductView.tsx
@@ -11,7 +11,7 @@ const ProductView = () => {
   const product = {
     name: "DUNK LOW RETRO",
     brand: "Nike Sportswear",
-    rating: "4.5",
+    rating: 4.5,
     price: 1200
   }
 
@@ -23,7 +23,7 @@ const ProductView = () => {
           <div className={styles.productInfo}>
             <h2 className={styles.productBrand}>{product.brand}</h2>
             <h1 className={styles.productTitle}>{product.name}</h1>
-            <p className={styles.productRating}>Rating: {product.rating}/5</p>
+            <p className={styles.productRating}>Rating: {product.rating}/5 ⭐</p>
             <p className={styles.productPrice}>{product.price} kr <span className={styles.vat}>incl. VAT</span></p>
           </div>
           <div className={styles.buttonContainer}>
