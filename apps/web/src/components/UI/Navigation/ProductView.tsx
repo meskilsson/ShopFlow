19c8ad5f27307edff32341c0093307b0415a414c
Marcus <marcuswilliamj@gmail.com>
2026-04-15 13:52:18 +0200

    modular-ish product viewer

--- apps/web/src/components/UI/Navigation/ProductView.tsx
+++ apps/web/src/components/UI/Navigation/ProductView.tsx
@@ -7,16 +7,24 @@ import HeartIconStd from "@/assets/icons/heart-regular-full.svg?react"
 import ButtonStd from '../ButtonStd'
 
 const ProductView = () => {
+
+  const product = {
+    name: "DUNK LOW RETRO",
+    brand: "Nike Sportswear",
+    rating: "4.5",
+    price: 1200
+  }
+
   return (
     <section className={styles.productContainer}>
       <img src={ProductImage} className={styles.productImage} />
 
       <div className={styles.sidebar}>
           <div className={styles.productInfo}>
-            <h2 className={styles.productBrand}>Nike Sportswear</h2>
-            <h1 className={styles.productTitle}>DUNK LOW RETRO</h1>
-            <p className={styles.productRating}>Rating: 4,5/5</p>
-            <p className={styles.productPrice}>1200 kr incl. VAT</p>
+            <h2 className={styles.productBrand}>{product.brand}</h2>
+            <h1 className={styles.productTitle}>{product.name}</h1>
+            <p className={styles.productRating}>Rating: {product.rating}/5</p>
+            <p className={styles.productPrice}>{product.price} kr <span className={styles.vat}>incl. VAT</span></p>
           </div>
           <div className={styles.buttonContainer}>
               <ButtonStd variant='primary' fullWidth text={"Add to cart"}/>
