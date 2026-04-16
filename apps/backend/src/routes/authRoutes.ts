290c05627d3404e453f471a433151fb9978bb26a
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-04-16 11:13:13 +0200

    Imported authorizeRoles that can check authorization based on roles, also added 4 endpoints of tests to check each role

--- apps/backend/src/routes/authRoutes.ts
+++ apps/backend/src/routes/authRoutes.ts
@@ -1,10 +1,32 @@
 import { Router } from "express";
 import { loginUser, getProfile } from "../controllers/authController";
 import authenticateToken from "../middleware/authenticateToken";
+import { authorizeRoles } from "../middleware/authorizeRoles";
 
 const authRouter = Router();
 
 authRouter.post("/login", loginUser);
-authRouter.get("/profile", authenticateToken, getProfile);
+authRouter.get("/profile", authenticateToken, authorizeRoles("admin", "buyer", "seller"), getProfile);
+
+// TEST ROUTES!
+
+authRouter.get("/buyer-area", authenticateToken, authorizeRoles("buyer"), (req, res) => {
+    res.json({ message: "Welcome buyer!", user: req.user });
+});
+
+authRouter.get("/seller-area", authenticateToken, authorizeRoles("seller"), (req, res) => {
+    res.json({ message: "Welcome seller!", user: req.user });
+});
+
+authRouter.get("/admin-area", authenticateToken, authorizeRoles("admin"), (req, res) => {
+    res.json({ message: "Welcome Admin!", req: req.user });
+});
+
+authRouter.get("/management-area", authenticateToken, authorizeRoles("seller", "admin"), (req, res) => {
+    res.json({ message: "Welcome seller/admin", req: req.user });
+});
+
+//END OF TEST ROUTES
+
 
 export default authRouter;
