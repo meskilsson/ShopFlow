2b64903e631badffef0e095bfa993579b7a27efc
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-04-21 09:43:20 +0200

    added an onClick on Log Out that logsout the user

--- apps/web/src/features/navbar/NavBar.tsx
+++ apps/web/src/features/navbar/NavBar.tsx
@@ -2,30 +2,36 @@ import styles from "./NavBar.module.css"
 import HeartIcon from "@/assets/icons/heart-solid-full.svg?react"
 import CartIcon from "@/assets/icons/cart-shopping-solid-full.svg?react"
 import ProfileIcon from "@/assets/icons/circle-user-solid-full.svg?react"
+import { useAuth } from "@/contexts/AuthContext"
 
 import Dropdown from "@/components/ui/Dropdown"
 
 const navbar = () => {
+
+  const { logout } = useAuth();
+
   return (
     <div className={styles.navbar}>
       <div className={styles.container}>
         <h2>ShopFlow</h2>
         <div className={styles.iconContainer}>
           <Dropdown>
-              <Dropdown.Trigger asChild>
-                {/* <ButtonStd variant='ghost' fullWidth>hej</ButtonStd> */}
-                <a className={styles.iconBtn}><ProfileIcon className={styles.icon}/></a>
-              </Dropdown.Trigger>
+            <Dropdown.Trigger asChild>
+              {/* <ButtonStd variant='ghost' fullWidth>hej</ButtonStd> */}
+              <a className={styles.iconBtn}><ProfileIcon className={styles.icon} /></a>
+            </Dropdown.Trigger>
 
-              <Dropdown.Content>
-                <button>Profile</button>
-                <button>Settings</button>
-                <button>Log out</button>
-              </Dropdown.Content>
-              
-            </Dropdown>
-          <a className={styles.iconBtn}><HeartIcon className={styles.icon}/></a>
-          <a className={styles.iconBtn}><CartIcon className={styles.icon}/></a>
+            <Dropdown.Content>
+              <button>Profile</button>
+              <button>Settings</button>
+              <button
+                onClick={() => logout()}
+              >Log out</button>
+            </Dropdown.Content>
+
+          </Dropdown>
+          <a className={styles.iconBtn}><HeartIcon className={styles.icon} /></a>
+          <a className={styles.iconBtn}><CartIcon className={styles.icon} /></a>
         </div>
       </div>
     </div>
