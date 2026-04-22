c1d86b8546936edc557570671c61dba04925fff8
Marcus <marcuswilliamj@gmail.com>
2026-04-22 13:50:48 +0200

    Smal fixxx

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -192,7 +192,10 @@ const HomePage = () => {
                 />
             </ProductsCarousel>
         </div>
-        <Hero title="KATTARP" subtitle="Have you tried the new Kattarp-collection?" image='https://images.unsplash.com/photo-1544441893-675973e31985?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+        <Hero title="KATTARP" subtitle="Have you tried the new Kattarp-collection?" image='https://images.unsplash.com/photo-1544441893-675973e31985?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <div style={{marginTop: "1rem"}}></div>
+            <ButtonStd variant='primary'>Check it out</ButtonStd>
+        </Hero>
         
       </Container>
     </>
