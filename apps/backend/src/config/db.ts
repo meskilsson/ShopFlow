74781f4ff66d2fffb1a737590b45f3be6f400ca8
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-04-28 16:57:57 +0200

    added guards to connectDB

--- apps/backend/src/config/db.ts
+++ apps/backend/src/config/db.ts
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
 export async function connectDB(): Promise<void> {
+
+  const mongoUri = process.env.MONGODB_URI;
+  const dbName = process.env.DB_NAME;
+
+  if (!mongoUri) {
+    throw new Error("MONGODB_URI is missing in environment variables");
+  }
+  if (!dbName) {
+    throw new Error("DB_NAME is missing in environment variables");
+  }
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string, {
-      dbName: process.env.DB_NAME,
+    await mongoose.connect(mongoUri, {
+      ...(dbName ? { dbName } : {}),
     });
 
     console.log("MongoDB Connected!");
