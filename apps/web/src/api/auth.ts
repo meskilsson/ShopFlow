8b70cdb87260fa5070d5a44b0992fa8a0fc200f2
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-05-07 09:51:55 +0200

    data.error added to Error

--- apps/web/src/api/auth.ts
+++ apps/web/src/api/auth.ts
@@ -1,6 +1,5 @@
 import type { LoginRequestInput } from "@/types/authTypes";
 
-
 const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5000/api/v1";
 
 export async function loginRequest(userData: LoginRequestInput) {
@@ -15,7 +14,7 @@ export async function loginRequest(userData: LoginRequestInput) {
     const data = await response.json();
 
     if (!response.ok) {
-        throw new Error(data.message || "Failed to login user");
+        throw new Error(data.message || data.error || "Failed to login user");
     }
 
     return data;
