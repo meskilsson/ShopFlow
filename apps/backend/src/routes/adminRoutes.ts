caf30a2fdcd9eee7913edfb52b0014eabcec2de3
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-05-13 12:05:41 +0200

    test route added and tested roles and auth with postman

---
+++ apps/backend/src/routes/adminRoutes.ts
@@ -0,0 +1,29 @@
+import Router from 'express';
+import { authorizeRoles } from '../middleware/authorizeRoles';
+import { validateRequest } from '../middleware/validate';
+import { requireAuth } from '../middleware/requireAuth';
+
+
+const adminRouter = Router();
+
+adminRouter.get("/", (req, res) => {
+    res.json({ message: "Logged in as admin " });
+});
+
+/* 
+
+/api/v1/admin/users
+/api/v1/admin/products
+/api/v1/admin/orders
+/api/v1/admin/reviews
+/api/v1/admin/dashboard
+
+GET /api/v1/admin/users
+GET /api/v1/admin/users/:id
+DELETE /api/v1/admin/users/:id
+PATCH /api/v1/admin/users/:id/restore
+
+*/
+
+
+export default adminRouter;
