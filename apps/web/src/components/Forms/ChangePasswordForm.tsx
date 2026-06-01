4cc745bd8092084399b94fe4cc68bf48d086654a
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-06-01 13:17:51 +0200

    removed duplicate getErrorMessage

--- apps/web/src/components/Forms/ChangePasswordForm.tsx
+++ apps/web/src/components/Forms/ChangePasswordForm.tsx
@@ -4,7 +4,6 @@ import { useAuth } from "@/contexts/AuthContext";
 import { useState } from "react";
 import ButtonStd from "../UI/ButtonStd";
 
-import { getErrorMessage } from "@/utils/getErrorMessage";
 
 export default function ChangePasswordForm() {
     const { user: authUser } = useAuth();
@@ -49,15 +48,7 @@ export default function ChangePasswordForm() {
 
             setSuccess("Password updated successfully.");
         } catch (error) {
-<<<<<<< HEAD
-            if (error instanceof Error) {
-                setError(getErrorMessage(error) || "Password could not be updated");
-            } else {
-                setError("Something went wrong");
-            }
-=======
             setError(getErrorMessage(error));
->>>>>>> a686e470b5ecfeda51b7ba17fb91c9e41e75b622
         } finally {
             setIsLoading(false);
         }
